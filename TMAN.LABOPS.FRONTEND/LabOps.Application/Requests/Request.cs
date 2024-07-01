a7a366d346b40faf6f27e76fd0050bcbf4d993a3
zGuian <Guianmarcos32@gmail.com>
2024-07-01 10:31:06 -0300

    Delete project WebFront

--- TMAN.LABOPS.FRONTEND/LabOps.Application/Requests/Request.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LabOps.Application.Requests
-{
-    public abstract class Request<T> where T : class
-    {
-        public Guid IdRequest { get; set; } = Guid.NewGuid();
-        public object? Dados { get; set; }
-    }
-}
